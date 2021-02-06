379b385142887ea4f8bc8957965d854d57540b05
Linus Käll <linus.kall.business@gmail.com>
2021-02-06 13:24:36 +0100

    It works

--- index.js
+++ index.js
@@ -36,16 +36,38 @@ fs.readdir(`${__dirname}/data/cover_letters`, (err, files) => {
         render(`cover-letter/${name}`, name);
     });
 });
+
+.line-bullet {
+    width: 40px;
+    height: 40px;
+    background-color: rgba(30,30,30,1);
+    border-radius: 20px;
+    text-align: center;
+    font-size: 20px;
+    z-index: 10000;
+    color: white;
+    padding-top: 0px;
+    margin: auto;
+    align-self: start;
+}
+.line-bullet > i {
+    padding-top: 10px;
+    padding-left: 2px;
+}
 */
+
+
+
 (async () => {                                  // declare function
     const browser = await puppeteer.launch();     // run browser
     const page = await browser.newPage();         // create new tab
     await page.goto('http://localhost:8080/data/resumes/');  // go to page
 
     await page.pdf({
-        path: 'page.pdf',
+        path: 'Linus_Käll_CV.pdf',
         format: "A4",
-        scale: 0.95
+        printBackground: true,
+        scale: 0.93
     });
     await browser.close();                        // close browser
   })();
