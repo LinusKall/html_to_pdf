fb4f6b1015aee5d7975be36e6bbd15e5d9de2ac9
Linus Käll <linus.kall.business@gmail.com>
2022-02-13 15:05:33 +0100

    No longer requires multiple terminals to run

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const connect = require('connect');
+const serveStatic = require('serve-static');
+
+connect()
+    .use(serveStatic(__dirname))
+    .listen(8080, () => {});
 
 // Look for folders in these subdirectories
 const dirs_to_render = ["/data/general", "/data/resumes", "/data/cover_letters"];
@@ -17,40 +23,56 @@ const render = async (path, file_name) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto(`http://localhost:8080/${path}`, { waitUntil: 'networkidle2' }).catch("Could not go to http://localhost:8080/${path}");
-    await page.pdf({ 
-        path: `${__dirname}/pdfs/${file_name}.pdf`, 
-        format: "A4", // "A4" standard, you can tweak this!
-        scale: 0.93 // 0.93 standard, you can tweak this!
-    }).catch("Could not render http://localhost:8080/${path}");
+    await page
+        .goto(`http://localhost:8080/${path}`, { waitUntil: 'networkidle2' })
+        .catch("Could not go to http://localhost:8080/${path}");
+    await page
+        .pdf({ 
+            path: `${__dirname}/pdfs/${file_name}.pdf`, 
+            format: "A4", // "A4" standard, you can tweak this!
+            scale: 0.93   // 0.93 standard, you can tweak this!
+        })
+        .catch("Could not render http://localhost:8080/${path}");
 
     await browser.close();
+    
+    return Promise.resolve(0);
 }
 
 // Render all HTML instances in dir_to_render
 const renderDir = async (dir_to_render) => {
-    fs.readdir(__dirname + dir_to_render, (err, files) => {
+    fs.readdir(__dirname + dir_to_render, async (err, files) => {
         if (err) {
             console.error(err);
             return;
         }
     
-        files.forEach((file) => {
+        return Promise.all(files.map(async (file) => {
             if (
                 file != ".gitignore" && 
                 file != "put your folders containing your HTML resumes here!" &&
                 file != "put your folders containing your HTML cover letters here!" &&
                 file != "put your folders containing your HTML here!"
             ) {
-                console.log(file);
                 const name = file.replace(/\.[^/.]+$/, "");
-                render(`${dir_to_render + "/" + name}/index.html`, name);
+                const res = await render(`${dir_to_render + "/" + name}/index.html`, name);
+                console.log("Rendered " + file);
+                return res;
             }
-        });
+        }));
     });
 }
 
 // Render all HTML instances in all dirs_to_render
 (async () => {
-    dirs_to_render.forEach(async (dir) => { renderDir(dir) } );
+    await Promise.all(dirs_to_render.map(async (dir) => { 
+        return renderDir(dir);
+    })).then(() => {
+        console.log("Check out the pdfs/ folder!");
+    }).catch(() => {
+
+    });
+    
 })();
+
+
